0a9873e1c28a1aba9c0e01a6612f5a4b3aa2c126
Reshmirajs <reshmirajs.attukal04@gmail.com>
2026-03-20 20:45:39 +0530

    Configure dotenv for local environment variables

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "i@mReshm1"
-MYSQL_DB = "hotel_db"
+import os
+from dotenv import load_dotenv
+
+# Load local .env file if it exists (ignored in Vercel production)
+load_dotenv()
+
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "i@mReshm1")
+MYSQL_DB = os.environ.get("MYSQL_DB", "hotel_db")
